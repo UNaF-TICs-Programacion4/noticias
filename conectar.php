15fbafe1815e36b9d5bea41bf55bbcc6e10b26f8
Heber <hebercaballero@gmail.com>
2016-05-19 11:07:08 -0300

    Creacion de la Clase Dd_Noticia, la cual toma los datos de config.cfg para realizar la conexión. Se deberia ignorar el envio de este archivo a GitHub.

--- conectar.php
+++ conectar.php
@@ -1,21 +1,25 @@
 <?php
 
-$servername = "localhost";
-$username = "prog4";
-$password = "prog4";
+/*
+*	Incluir el archivo config.cfg con los parametros de la conexión.
+*/
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=db_noticias", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa.";
-    }
-catch(PDOException $e)
-    {
-    echo "Falló la conexión: " . $e->getMessage();
-    }
+include "config.cfg";
 
-// Para prueba de conexión. Después quitar.
-// $conn = null;
+Class Db_Noticia {
+
+
+	public function __construct() {
+		try {
+		    $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+		    // set the PDO error mode to exception
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    echo "Conexión exitosa.";
+		}
+		catch(PDOException $e){
+		    echo "Falló la conexión: " . $e->getMessage();
+		    }
+	}
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include_once "conectar.php";
+    $db = new Db_Noticia();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
